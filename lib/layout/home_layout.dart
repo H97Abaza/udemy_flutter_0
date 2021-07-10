dfaa0289838118ae58f9d23052589fc6cdeea85d
ADIGA612 <h97abaza@gmail.com>
2021-07-10 21:48:57 +0300

    commit on 'todo_app'

--- lib/layout/home_layout.dart
+++ lib/layout/home_layout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
 import 'package:udemy_flutter/modules/archived_tasks/archived_tasks_screen.dart';
 import 'package:udemy_flutter/modules/done_tasks/done_tasks_screen.dart';
 import 'package:udemy_flutter/modules/new_tasks/new_tasks_screen.dart';
@@ -22,6 +23,13 @@ class _HomeLayoutState extends State<HomeLayout> {
     'Done Tasks',
     'Archived Tasks',
   ];
+
+  @override
+  void initState() {
+    super.initState();
+    createDatabase();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,7 +37,24 @@ class _HomeLayoutState extends State<HomeLayout> {
         title: Text(titles[currentTabIndex]),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () async {
+          try {
+            print(await getName());
+            print('Kasem');
+            throw ('some errors');
+          } catch (error) {
+            print('error ${error.toString()}');
+          }
+          /*  getName().then(
+            (value) {
+              print(value);
+              print('Kasem');
+              throw ('some errors');
+            } ,
+          ).catchError((e) => print('error ${e}'));
+          
+        }*/
+        },
         child: Icon(
           Icons.add,
         ),
@@ -60,4 +85,31 @@ class _HomeLayoutState extends State<HomeLayout> {
       body: screens[currentTabIndex],
     );
   }
+
+  Future<String> getName() async {
+    return 'Hussein Abaza';
+  }
+
+  void createDatabase() async {
+    var database = await openDatabase('todo.db', version: 1,
+        onCreate: (database, version) {
+      print('database created');
+      //id integer
+      //title string
+      //date string
+      //time string
+      //status string
+      database
+          .execute(
+              'CREATE TABLE tasks (id INTEGER PRIMARY KEY,title TEXT,date TEXT,time TEXT,status TEXT)')
+          .then((value) => print('table created'))
+          .catchError((error) {
+        print('Error when creating table: ${error.toString()}');
+      });
+    }, onOpen: (database) {
+      print('database opened');
+    });
+  }
+
+  void insertToDatabase() {}
 }
